9832ddeb900c9a330fd0fdcac74b39c0753e790b
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-03-04 15:27:05 +0100

    Add output directory for ogs test

--- test_ogs.sh
+++ test_ogs.sh
@@ -3,5 +3,5 @@
 
 start=$(date +%s)
 
-ogs model_inputs/Model_ML_IRZ_test/MULTI_BW_line_IRZ.prj
+ogs model_inputs/Model_ML_IRZ_test/MULTI_BW_line_IRZ.prj -o Tests/outputs/MultiLayer_test
 #ogs model_inputs/test/OneLayer_Coarse/OneLayer_T1e2.prj -o Tests/outputs/MultiLayer_test
