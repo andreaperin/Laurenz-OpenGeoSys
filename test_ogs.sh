b34a06cdc8ed81dd075c23eb6409302e24618a13
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-03-09 09:44:03 +0100

    New foldering

--- test_ogs.sh
+++ test_ogs.sh
@@ -3,4 +3,6 @@
 
 start=$(date +%s)
 
-OGS_LOG_LEVEL=error /home/perin/Documents/projects/work/code/ogs/build/bin/ogs model_inputs/OneLayer_IRZ_Coarse_Refined_mesh_test/OneLayer_IRZ_T1e2_konstVisk.prj -o Tests/outputs/OneLayer_test
+#OGS_LOG_LEVEL=error /home/perin/Documents/projects/work/code/ogs/build/bin/ogs model_inputs/OneLayer_Coarse_test/OneLayer_T1e2.prj -o Tests/outputs/OneLayer_test
+
+/home/perin/Documents/projects/work/code/ogs/build/bin/ogs model_inputs/Model_ML_IRZ/MULTI_BW_line_IRZ.prj -o Tests/outputs/OneLayer_test
