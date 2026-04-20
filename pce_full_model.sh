acc73f9cddf705cc50fd7f8bbea620b7e56d165d
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-04-20 20:36:21 +0200

    Add correction for running in the cluster

--- pce_full_model.sh
+++ pce_full_model.sh
@@ -6,4 +6,8 @@
 #SBATCH --output=pce_fullmodel_stdoutput.txt
 #SBATCH --error=pce_fullmodel_stderror.txt
 
+module load OpenGeoSys
+
+export OMP_NUM_THREADS=1
+
 julia -p $SLURM_CPUS_PER_TASK --project=/home/andrea.perin/ThermoOptiPlan -e 'using Pkg; Pkg.instantiate(); include("./new_pce.jl")'
