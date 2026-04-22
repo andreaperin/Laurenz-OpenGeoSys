7c80095fb9d82704c99ec9bbde2152bbcd165ebd
Andrea Perin <andrea.perin@irz.uni-hannover.de>
2026-04-22 14:04:58 +0200

    rename new_pce to pce

--- pce_full_model.sh
+++ pce_full_model.sh
@@ -10,4 +10,4 @@ module load OpenGeoSys
 
 export OMP_NUM_THREADS=1
 
-julia -p $SLURM_CPUS_PER_TASK --project=/home/andrea.perin/ThermoOptiPlan -e 'using Pkg; Pkg.instantiate(); include("./new_pce.jl")'
+julia -p $SLURM_CPUS_PER_TASK --project=/home/andrea.perin/ThermoOptiPlan -e 'using Pkg; Pkg.instantiate(); include("./pce.jl")'
